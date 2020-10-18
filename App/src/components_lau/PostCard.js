5b5ac6c6fc04dec2412df166951d1e9bb42f6fa9
laudvg <laura.vivas.gamboa@gmail.com>
2020-10-18 15:05:14 +0200

    finished!

--- App/src/components_lau/PostCard.js
+++ App/src/components_lau/PostCard.js
@@ -3,6 +3,7 @@ import { useQuery } from '@apollo/client';
 import {GET_POSTS} from '../services/apiQueries.js';
 import ErrorComponent from './Error'
 import Loading from './Loading.js';
+import UseUserName from './UserName'
 
 
 const UseCreatePostcard = () => {
@@ -13,7 +14,7 @@ const UseCreatePostcard = () => {
 
   return(
     <div className="w-full rounded-lg lg:rounded-r-lg lg:rounded-l-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0 px-12 h-full overflow-auto">
-      <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start mt-4">{data.user.name}'s Posts:</p>
+      <UseUserName/>
       <ul>
         {data.user.posts.data.map(({title, id}) => (
           <li className="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start" key={id}>Title: {title}</li>
---
+++ App/src/components_lau/UserName.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_USER } from '../services/apiQueries.js';
+import ErrorComponent from './Error'
+import Loading from './Loading.js';
+
+const UseUserName = () => {
+  const { loading, error, data } = useQuery(GET_USER);
+
+  if (loading) return <Loading/>;
+  if (error) return <ErrorComponent />;
+
+  return (
+      <div>
+          <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start mt-4">{data.user.name}'s Posts:</p>
+      </div>
+  )
+
+
+}
+
+export default UseUserName;
